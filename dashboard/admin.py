8f9ac210caea63cf36c8d2646036f7df87055ecd
John Huddleston <huddlej@gmail.com>
2014-04-21 21:37:34 -0700

    make the admin interface more useful.

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from models import Job
 
-admin.site.register(Job)
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ("owner", "jobname", "jobnumber", "slots", "ru_maxrss", "ru_wallclock", "exit_status")
+    list_filter = ("owner", "slots", "exit_status")
+
+admin.site.register(Job, JobAdmin)
