2f1f3f4d9ce56cb6a500a077fbf15da74f0b08be
John Huddleston <huddlej@gmail.com>
2014-04-23 21:30:57 -0700

    added a view and template for the index page showing top users by job attributes.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Count, Sum
+from django.views.generic import ListView
 
-# Create your views here.
+from models import Job
+
+
+class JobListView(ListView):
+    model = Job
+
+    def get_queryset(self):
+        order_by = "-%s" % self.request.GET.get("order_by", "total_memory")
+        return Job.objects.values("owner").annotate(total_memory=Sum("ru_maxrss"), total_slots=Sum("slots"), total_jobs=Count("jobnumber")).order_by(order_by)
--- sge_dashboard/urls.py
+++ sge_dashboard/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sge_dashboard.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from dashboard.views import JobListView
 
+urlpatterns = patterns('',
+    url(r'^$', JobListView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
